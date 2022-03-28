e58c761b5c535d82bdbc82245c50919328e5a8d4
Evie <evangeliapal@gmail.com>
2022-03-28 12:45:51 +0100

    A function that uses a callback to return parsed JSON data

---
+++ github.js
@@ -0,0 +1,25 @@
+//const got = require('got');
+import('got');
+
+const handleReceivedResponse = (response) => {
+  const responseObject = JSON.parse(response.body);
+  console.log(responseObject);
+}
+
+got.get('https://api.github.com/repos/sinatra/sinatra').then(handleReceivedResponse);
+
+
+// Same, but wrapped in a function to work for any repo, plus a callback function as a way to "return" or "pass back" the responseObject to the function's caller.
+const got = require('got');
+
+const fetchRepoInfo = (repoName, callback) => {
+  got(`https://api.github.com/repos/${repoName}`).then((response) => {
+    const responseObject = JSON.parse(response.body);
+    callback(responseObject);
+  });
+}
+
+// The function would then be called this way:
+fetchRepoInfo('sinatra/sinatra', (repoResponseObject) => {
+  console.log(repoResponseObject);
+});
